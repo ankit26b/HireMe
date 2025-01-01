b66a69517ef216034b5a823b3b7ba8869826aa75
Ankit Barmase <ankitbarmase26@gmail.com>
2025-01-01 12:37:15 +0530

    init commit

---
+++ src/components/header.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
+
+const Header = () => {
+  const [showSignIn, setshowSignIn] = useState(false);
+
+  const [search, setSearch] = useSearchParams();
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (search.get("sign-in")) {
+      setshowSignIn(true);
+    }
+  }, [search]);
+
+  const handleOverlayClick = (e) => {
+    setshowSignIn(false);
+  };
+
+  return (
+    <>
+      <nav className="py-4 flex justify-between items-center">
+        <Link>
+          <img src="/HireMe-logo.png" className="h-20" alt="" />
+        </Link>
+
+        <div className="flex gap-8">
+          <SignedOut>
+            <Button varianr="outline" onClick={() => setshowSignIn(true)}>
+              Login
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            {user?.unsafeMetadata?.role === "recruiter" && (<Link to="/post-job">
+              <Button variant="destructive" className="rounded-full">
+                <PenBox size={20} className="mr-2" />
+                Post a Job
+              </Button>
+            </Link>)}
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            >
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My Jobs"
+                  labelIcon={<BriefcaseBusiness size={15} />}
+                  href="/my-jobs"
+                />
+                <UserButton.Link
+                  label="Saved Jobs"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-jobs"
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
+        </div>
+      </nav>
+
+      {showSignIn && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverlayClick}
+        >
+          <SignIn
+            signUpFallbackRedirectUrl="/onboarding"
+            fallbackRedirectUrl="/onboarding"
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Header;
---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,19 @@
+
+import Header from '@/components/header';
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+const AppLayout = () => {
+  return (
+    <div>
+        <div className="grid-background"></div>
+        <main className='min-h-screen container px-8'>
+          <Header/>
+          <Outlet/>
+        </main>
+        <div className='p-10 text-center bg-gray-800 mt-10'>Made by Ankit</div>
+    </div>
+  )
+}
+
+export default AppLayout;
